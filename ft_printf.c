347128c524a76889f955d45008e1d6029df490db
Diego Vargas <divaro_@hotmail.com>
2025-11-24 20:01:33 +0100

    asd

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dievarga <dievarga@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 18:29:20 by dievarga          #+#    #+#             */
-/*   Updated: 2025/11/22 22:33:02 by dievarga         ###   ########.fr       */
+/*   Updated: 2025/11/24 19:19:53 by dievarga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,24 +36,26 @@ static void	ft_detect(const char **p, va_list *args, int *count)
 	}
 }
 
-static void	ft_printchar(char c, int *count)
-{
-	write(1, &c, 1);
-	(*count)++;
-}
-
 static void	ft_scan(const char **fmt, va_list *args, int *count)
 {
 	if (**fmt == '%')
 	{
 		(*fmt)++;
 		if(**fmt == '%')
-			ft_printchar('%', count);
+		{
+			write(1, "%", 1);
+			(*count)++;
+			return ;
+		}
 		else if (**fmt)
 			ft_detect(fmt, args, count);
+		return ;
 	}
 	else
-		ft_printchar(**fmt, count);
+	{
+		write(1, *fmt, 1);
+		(*count)++;
+	}
 }
 
 int	ft_printf(const char *format, ...)
@@ -75,22 +77,38 @@ int	ft_printf(const char *format, ...)
 
 /*
 ft_printf function takes a format especifier 
-undefined number of times telling us what
-will be printed.
-As a variadic function it also needs to know
-how many arguments it has in this case, by
-seeing how many "%..." there are as args.
+for an undefinedi number of times telling us
+what will be printed.
+As a variadic function with the va_list args
+it can 
 it initializes the va_list type args and cpy
 and va_starts initializes the first argument
 
 
 */
-/*
+
 #include <stdio.h>
 int	main(void)
 {
-	int	a = ft_printf("ft_printf prints %c %s %d %i %u %p %x %X\n",'a', "hola", -10, -214, 5, "asd", 123, 123);
-	int	b = printf("og printf prints %c %s %d %i %u %p %x %X\n", 'a', "hola", -10, -214, 5, "asd", 123, 123);
+	int	a = ft_printf("ft_printf prints %% %%j %c %s %d %i %u %p %x %X\n",'a', "hola", -10, -214, 5, "asd", 123, 123);
+	int	b = printf("og printf prints %% %%j %c %s %d %i %u %p %x %X\n", 'a', "hola", -10, -214, 5, "asd", 123, 123);
 	printf("ft_printf returns %d chars\nog printf returns %d chars\n", a, b);
+
+	ft_printf("pointer test %p\n", (void *)"asd");
+	printf("pointer test %p\n", (void *)"asd");
+	ft_printf("char test %c\n", '0');
+	printf("char test %c\n",'0');
+	ft_printf("string test %s\n", "asd");
+	printf("string test %s\n", "asd");
+	ft_printf("decimal test %d\n", -2147483647);
+	printf("decimal test %d\n", -2147483647);
+	ft_printf("integer test %i\n", -2147483647);
+	printf("integer test %i\n", -2147483647);
+	ft_printf("unsigned test %u\n", -1);
+	printf("unsigned test %u\n", -1);
+	ft_printf("lower hex test %x\n", -224748364);
+	printf("lower hex test %x\n", -224748364);
+	ft_printf("upper hex test %X\n", -1);
+	printf("upper hex test %X\n", -1);
 	return (0);
-}*/
+}
